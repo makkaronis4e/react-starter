fabf157c7f29ca9943f1273482d01e774d464d2d
makkaronis4e <makkaronis4e@gmail.com>
2018-04-12 11:53:29 +0300

    Add refresh button

--- src/modules/App/Jokes/Filters.js
+++ src/modules/App/Jokes/Filters.js
@@ -12,10 +12,12 @@ export default class Filters extends PureComponent {
       <div className="options-block">
         <div className="jokes-number">
           Number of jokes: <input id="jokes-number" type="number" min="1" max="10" value="1" />
+          <button className="refresh">Refresh</button>
         </div>
         <ul>
           {categoryItems}
           <button className="cat-button cat-button-nocategory">No category</button>
+          <button className="cat-button cat-button-all">Select All</button>
         </ul>
       </div>
     );
